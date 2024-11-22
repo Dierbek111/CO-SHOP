0615f2f64bbc5363f97d7493df459e77cd9c5ddf
Dierbek111 <dierbekrahmatullaev00@gmail.com>
2024-11-22 18:56:29 +0500

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import Hero from '../components/Hero/Hero'
+import Category from '../components/Category/Category'
+
+const Home = () => {
+  return (
+    <>
+    <Header/>
+    <Hero/>
+    <Category/>
+    </>
+  )
+}
+
+export default Home
